97165a257ad8523bf3aefd54fc816a20ba816a87
RafaScriptter <nativaeleven@gmail.com>
2026-07-02 15:55:33 -0300

    Modal concluido

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import { tooltip } from "./tooltip.js";
 import { tabNav } from "./tabNav.js";
 import { faq } from "./faq.js";
 import { dropDownMenu } from "./dropDown.js";
+import { modal } from "./modal.js";
 
 tooltip();
 tabNav();
 faq();
 dropDownMenu();
+modal();
